dcdf5b6e6009ad808e293d4f8ead5d5cbee38887
gabriel ferreira <gf2627651@gmail.com>
2022-05-24 11:50:26 -0300

    dia2 prova

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 
-import express from 'express'
+import express, { response } from 'express'
 import cors from'cors'
 
 
@@ -99,6 +99,48 @@ server.get('/temperatura', (req,resp)=>{
         }
         
     }catch(err){
+        resp.status(404).send({
+            erro: err.message
+        })
+    }
+})
+
+server.get('/DIA2/corprimaria/:cor' , (req,resp) =>{
+    try {
+        const cor = req.params.cor;
+        if(cor === "amarelo"|| cor === "azul" || cor === "vermelho"){
+            resp.send({
+                cor:"true"
+            })
+        }else{
+            resp.send({
+                cor:"false"
+            })
+        }
+    } catch (err) {
+        resp.status(404).send({
+            erro: err.message
+        })
+    }
+})
+
+server.post('/DIA2/ingressoCinema' , (req,resp) =>{
+    try {
+        const { valores : {qtdint , qtdMeia , diaSemana , nacionalidade}} = req.body;
+        if(nacionalidade === "brasileira"){
+            resp.send({
+                total : (qtdint + qtdMeia) * 5.00
+            })
+        }else{
+            if(diaSemana === "quarta"){
+                resp.send({
+                    total : (qtdint + qtdMeia) * 14.25
+                })
+            }else{
+                total (qtdMeia * 14.25) + (qtdint * 28.5)
+            }
+        }
+    } catch (err) {
         resp.status(404).send({
             erro: err.message
         })
